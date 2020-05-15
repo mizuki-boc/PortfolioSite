382a5563aad705be536292319b445777078851e4
mizuki-boc <mizuki0842boc@icloud.com>
2020-05-15 12:52:00 +0900

    add research_paper

--- manage.py
+++ manage.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/research")
+def research():
+    return render_template("research.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8888, threaded=True)
